e2dd972d7deb924fbbf630ff098b3e62bfa05b1a
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-18 16:17:35 -0500

    added touch control

--- js/main.js
+++ js/main.js
@@ -2,17 +2,28 @@
 const canvas = document.getElementById('board');
 const ctx = canvas.getContext('2d');
 const scoreElement = document.getElementById('score');
+const levelElement = document.getElementById('level');
+const pauseButton = document.createElement('button');
+document.body.appendChild(pauseButton);
+pauseButton.innerText = "Pause";
+pauseButton.style.position = 'absolute';
+pauseButton.style.bottom = '10px';
+pauseButton.style.right = '10px';
 
 // Constants
 const BLOCK_SIZE = 20;
 const COLS = 12;
 const ROWS = 24;
+let dropInterval = 1000; // 1 second
+let level = 1;
+let linesCleared = 0;
 
 // Game Variables
 let board = Array.from({ length: ROWS }, () => Array(COLS).fill(0));
 let score = 0;
 let currentShape;
 let currentPos = { x: COLS / 2 - 2, y: 0 };
+let isPaused = false;
 
 // Loading Images
 const blockImages = [];
@@ -22,7 +33,6 @@ for (let i = 1; i <= 14; i++) {
     blockImages.push(img);
 }
 
-// Penta-shapes
 const pentaShapes = [
     // 5-long
     [[1, 1, 1, 1, 1]],
@@ -50,158 +60,67 @@ const pentaShapes = [
 
 ];
 
-// Rest of your code...
-
 function resetBoard() {
     board = Array.from({ length: ROWS }, () => Array(COLS).fill(0));
 }
 
-function drawBlock(x, y, blockType) {
-    ctx.drawImage(blockImages[blockType - 1], BLOCK_SIZE * x, BLOCK_SIZE * y, BLOCK_SIZE, BLOCK_SIZE);
-    ctx.strokeRect(BLOCK_SIZE * x, BLOCK_SIZE * y, BLOCK_SIZE, BLOCK_SIZE);
+function drawBlock(x, y, imageIndex) {
+    ctx.drawImage(blockImages[imageIndex], BLOCK_SIZE * x, BLOCK_SIZE * y, BLOCK_SIZE, BLOCK_SIZE);
 }
 
 function drawShape() {
     for (let y = 0; y < currentShape.length; y++) {
         for (let x = 0; x < currentShape[y].length; x++) {
             if (currentShape[y][x]) {
-                drawBlock(currentPos.x + x, currentPos.y + y, currentShape[y][x]);
+                drawBlock(currentPos.x + x, currentPos.y + y, currentShape[y][x] - 1);
             }
         }
     }
-}
-
-function generateRandomShape() {
-    const randomIndex = Math.floor(Math.random() * pentaShapes.length);
-    const baseShape = pentaShapes[randomIndex];
-    return baseShape.map(row => row.map(cell => cell ? getRandomBlockNumber() : 0));
-}
-
-function getRandomBlockNumber() {
-    return Math.floor(Math.random() * 14) + 1;
 }
 
 function spawnShape() {
-    currentShape = generateRandomShape();
+    currentShape = pentaShapes[Math.floor(Math.random() * pentaShapes.length)];
     currentPos = { x: COLS / 2 - 2, y: 0 };
 }
 
-
 function dropShape() {
-    // Check if moving down causes a collision
     if (!isCollision(board, currentShape, currentPos.x, currentPos.y + 1)) {
         currentPos.y++;
     } else {
-        // Merge the current shape into the board because it can't move down anymore
         mergeBoard(board, currentShape, currentPos.x, currentPos.y);
-
-        // Check for line clears here (if implemented)
-
-        // Spawn a new shape at the top
+        checkForLines();
         spawnShape();
-
-        // Optionally: Check if the new shape immediately collides, signaling a Game Over
         if (isCollision(board, currentShape, currentPos.x, currentPos.y)) {
-            // Handle game over (e.g., stop game loop, show game over message)
             console.log("Game Over");
-            // Here you can stop the game loop and provide other feedback to the player.
-            return;
         }
     }
 }
 
-let touchStartX = 0;
-let touchStartY = 0;
+function checkForLines() {
+    for (let y = board.length - 1; y >= 0; y--) {
+        if (board[y].every(cell => cell)) {
+            board.splice(y, 1);
+            board.unshift(Array(COLS).fill(0));
+            y++;
+            score += 100;
+            linesCleared++;
 
-canvas.addEventListener('touchstart', (e) => {
-    touchStartX = e.touches[0].clientX;
-    touchStartY = e.touches[0].clientY;
-}, false);
-
-canvas.addEventListener('touchend', (e) => {
-    let deltaX = e.changedTouches[0].clientX - touchStartX;
-    let deltaY = e.changedTouches[0].clientY - touchStartY;
-
-    if (Math.abs(deltaX) > Math.abs(deltaY)) {
-        if (deltaX > 0) {
-            moveShape(1, 0);
-        } else {
-            moveShape(-1, 0);
-        }
-    } else {
-        if (deltaY > 0) {
-            while (!isCollision(board, currentShape, currentPos.x, currentPos.y + 1)) {
-                currentPos.y++;
+            if (linesCleared % 5 === 0) {
+                level++;
+                dropInterval *= 0.95;
             }
-        } else {
-            spawnShape();
+            scoreElement.textContent = "Score: " + score;
+            levelElement.textContent = "Level: " + level;
         }
     }
-}, false);
-
-canvas.addEventListener('click', () => {
-    rotateShape();
-});
-
-let lastTime = 0;
-const dropInterval = 1000;
-
-function gameLoop(timestamp) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    if (timestamp - lastTime > dropInterval) {
-        dropShape();
-        lastTime = timestamp;
-    }
-
-    drawShape();
-    requestAnimationFrame(gameLoop);
 }
-
-function displayAllShapes() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    pentaShapes.forEach((shape, index) => {
-        let x = index * 6;
-        let y = 5;
-
-        shape.forEach((row, rowIndex) => {
-            row.forEach((value, colIndex) => {
-                if (value) {
-                    drawBlock(x + colIndex, y + rowIndex);
-                }
-            });
-        });
-    });
-}
-
-function startGame() {
-    displayAllShapes();
-
-    setTimeout(() => {
-        resetBoard();
-        spawnShape();
-        gameLoop();
-    }, 5000);
-}
-
-startGame();
 
 function isCollision(board, shape, posX, posY) {
     for (let y = 0; y < shape.length; y++) {
         for (let x = 0; x < shape[y].length; x++) {
-            if (shape[y][x]) {
-                if (
-                    // Outside the game bounds
-                    typeof board[y + posY] === 'undefined' ||
-                    typeof board[y + posY][x + posX] === 'undefined' ||
-                    board[y + posY][x + posX] ||
-                    x + posX < 0 ||
-                    y + posY >= ROWS ||
-                    x + posX >= COLS
-                ) {
-                    return true;
-                }
+            if (shape[y][x] &&
+                (board[y + posY] && board[y + posY][x + posX]) !== 0) {
+                return true;
             }
         }
     }
@@ -225,15 +144,74 @@ function mergeBoard(board, shape, posX, posY) {
     }
 }
 
-
 function rotateShape() {
-    const newShape = currentShape[0].map((_, index) =>
-        currentShape.map(row => row[index])
-    ).reverse();
-
-    if (!isCollision(board, newShape, currentPos.x, currentPos.y)) {
-        currentShape = newShape;
+    const oldShape = currentShape;
+    currentShape = currentShape[0].map((_, index) => currentShape.map(row => row[index])).reverse();
+    if (isCollision(board, currentShape, currentPos.x, currentPos.y)) {
+        currentShape = oldShape;
     }
 }
 
+let lastTime = 0;
+function gameLoop(timestamp) {
+    if (!isPaused) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        if (timestamp - lastTime > dropInterval) {
+            dropShape();
+            lastTime = timestamp;
+        }
+        drawShape();
+    }
+    requestAnimationFrame(gameLoop);
+}
 
+pauseButton.addEventListener('click', () => {
+    isPaused = !isPaused;
+    pauseButton.innerText = isPaused ? "Resume" : "Pause";
+});
+
+canvas.addEventListener('touchstart', (event) => {
+    const touchX = event.touches[0].clientX;
+    const touchY = event.touches[0].clientY;
+    let touchStartX = 0;
+canvas.addEventListener('touchstart', (event) => {
+    touchStartX = event.touches[0].clientX;
+});
+
+canvas.addEventListener('touchend', (event) => {
+    const touchEndX = event.changedTouches[0].clientX;
+    const canvasRect = canvas.getBoundingClientRect();
+
+    if (touchEndX < canvasRect.left + canvasRect.width / 2) {
+        moveShape(-1, 0);  // Move left
+    } else {
+        moveShape(1, 0);  // Move right
+    }
+
+    if (touchEndX - touchStartX < -100) { // Swipe left
+        moveShape(-1, 0);
+    } else if (touchEndX - touchStartX > 100) { // Swipe right
+        moveShape(1, 0);
+    }
+});
+
+canvas.addEventListener('touchmove', (event) => {
+    const touchY = event.touches[0].clientY;
+    const canvasRect = canvas.getBoundingClientRect();
+
+    if (touchY > canvasRect.bottom) {
+        dropShape();
+    }
+});
+
+canvas.addEventListener('dblclick', () => {
+    rotateShape();
+});
+
+function startGame() {
+    resetBoard();
+    spawnShape();
+    gameLoop();
+}
+
+startGame();
