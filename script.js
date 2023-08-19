88ecf346c03c7c217c1f4305881f15590eb2afd3
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-19 17:41:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -54,14 +54,16 @@ function handleTouchEnd(event) {
     } else {
         // Vertical swipe
         if (deltaY > 0) {
-            // Swipe down
-            while (!checkCollision(currentShape, currentPos.x, currentPos.y + 1)) {
-                currentPos.y += 1; // Drop to the bottom
-            }
-            mergeShape();
-            currentShape = generateRandomShape();
-            currentPos = { x: Math.floor(COLS / 2) - 2, y: 0 };
-        } else {
+    // Swipe down
+    while (!checkCollision(currentShape, currentPos.x, currentPos.y + 1) && currentPos.y < ROWS - 1) {
+        currentPos.y += 1; // Drop to the bottom
+    }
+    mergeShape();
+    currentShape = generateRandomShape();
+    currentPos = { x: Math.floor(COLS / 2) - 2, y: 0 };
+}
+
+            } else {
             // Swipe up
             currentShape = generateRandomShape(); // Change to the next shape
         }
