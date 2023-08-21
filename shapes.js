4bdde4ca61cf32f9620a9de34d264f5136144b1d
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-21 03:30:03 -0500

    added colors to shapes.js

--- shapes.js
+++ shapes.js
@@ -1,7 +1,37 @@
 export { generateStraightShape, generateAShape, generateSquareShape, generateBShape, generateCShape, generateDShape, generateEShape, generateFShape, generateGShape, generateTShape, generateZShape, generateSShape, generateUShape, generateRandomShape };
+export const SQUARE_COLOR_INDEX = 1;
+export const T_SHAPE_COLOR_INDEX = 2;
+export const A_SHAPE_COLOR_INDEX = 3;
+export const B_SHAPE_COLOR_INDEX = 4;
+export const C_SHAPE_COLOR_INDEX = 5;
+export const D_SHAPE_COLOR_INDEX = 6;
+export const E_SHAPE_COLOR_INDEX = 7;
+export const F_SHAPE_COLOR_INDEX = 8;
+export const G_SHAPE_COLOR_INDEX = 9;
+export const Z_SHAPE_COLOR_INDEX = 10;
+export const S_SHAPE_COLOR_INDEX = 11;
+export const U_SHAPE_COLOR_INDEX = 12;
+export const STRAIGHT_COLOR_INDEX = 13;
+
+export const SHAPES_COLORS = [
+  null,  // No color for index 0
+  "#f00", // Red for shape 1
+  "#0f0", // Green for shape 2
+  "#00f", // Blue for shape 3
+  "#ff0", // Yellow for shape 4
+  "#0ff", // Cyan for shape 5
+  "#f0f", // Magenta for shape 6
+  "#f90", // Orange for shape 7
+  "#90f", // Violet for shape 8
+  "#09f", // Teal for shape 9
+  "#f09", // Rose for shape 10
+  "#966", // Brown for shape 11
+  "#6f9", // Lime Green for shape 12
+  "#609"  // Indigo for shape 13
+];
 
 function generateStraightShape() {
-    const segment = getRandomSegment();
+    const segment = STRAIGHT_COLOR_INDEX;
     return [
         [segment, segment, segment, segment, segment],
         [0, 0, 0, 0, 0],
