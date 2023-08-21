c821c788f055810e06db05eeaa0a236d7ec0d28e
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-21 02:34:11 -0500

    moved new shapes to separate js file

--- shapes.js
+++ shapes.js
@@ -1,2 +1,135 @@
 export { generateAShape, generateSquareShape, generateBShape, generateCShape, generateDShape, generateEShape, generateFShape, generateGShape, generateTShape, generateZShape, generateSShape, generateUShape, generateRandomShape };
 
+function generateAShape() {
+    const segment = A_SHAPE_COLOR_INDEX;
+    return [
+        [segment, segment, segment, segment, 0],
+        [0, segment, 0, 0, 0],
+        [0, , 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
+}
+
+function generateSquareShape() {
+    const segment = SQUARE_COLOR_INDEX;
+    return [
+        [0, 0, segment, segment, 0],
+        [0, 0, segment, segment, 0],
+        [0, 0, segment, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
+}
+
+function generateBShape() {
+    const segment = B_SHAPE_COLOR_INDEX;
+    return [
+        [segment, segment, segment, 0, 0],
+        [0, 0, segment, 0, 0],
+        [0, 0, segment, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
+}
+
+function generateCShape() {
+    const segment = C_SHAPE_COLOR_INDEX;
+    return [
+        [0, 0, segment, segment, 0],
+        [0, 0, segment, segment, 0],
+        [0, 0, 0, segment, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
+}
+
+function generateDShape() {
+    const segment = D_SHAPE_COLOR_INDEX;
+    return [
+        [0, segment, 0, 0, 0],
+        [segment, segment, segment, 0, 0],
+        [0, segment, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
+}
+
+function generateEShape() {
+    const segment = E_SHAPE_COLOR_INDEX;
+    return [
+        [segment, 0, 0, 0, 0],
+        [segment, 0, 0, 0, 0],
+        [segment, segment, segment, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
+}
+
+function generateFShape() {
+    const segment = F_SHAPE_COLOR_INDEX;
+    return [
+        [0, 0, segment, 0, 0],
+        [0, 0, segment, 0, 0],
+        [segment, segment, segment, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
+}
+
+function generateGShape() {
+    const segment = G_SHAPE_COLOR_INDEX;
+    return [
+        [segment, segment, 0, 0, 0],
+        [segment, 0, 0, 0, 0],
+        [segment, 0, 0, 0, 0],
+        [segment, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
+}
+
+
+function generateTShape() {
+    const segment = T_SHAPE_COLOR_INDEX;
+    return [
+        [0, segment, 0, 0, 0],
+        [segment, segment, segment, 0, 0],
+        [segment, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
+}
+
+function generateZShape() {
+    const segment = Z_SHAPE_COLOR_INDEX;
+    return [
+        [segment, segment, 0, 0, 0],
+        [0, segment, 0, 0, 0],
+        [0, segment, 0, 0, 0],
+        [0, segment, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
+}
+
+
+function generateSShape() {
+    const segment = S_SHAPE_COLOR_INDEX;
+    return [
+        [0, segment, segment, 0, 0],
+        [segment, segment, 0, 0, 0],
+        [0, segment, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
+}
+
+function generateUShape() {
+    const segment = U_SHAPE_COLOR_INDEX;
+    return [
+        [segment, 0, segment, 0, 0],
+        [segment, segment, segment, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
+}
