60041cf8ac2cc8caf5a9d95b98af2750c675bde7
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-21 02:33:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { generateAShape, generateSquareShape, generateBShape, generateCShape, generateDShape, generateEShape, generateFShape, generateGShape, generateTShape, generateZShape, generateSShape, generateUShape, generateRandomShape } from './shapes.js';
+
 const COLS = 30; // Increase the number of columns
 const ROWS = 35; // Increase the number of rows
 const canvas = document.getElementById('board');
@@ -147,142 +149,8 @@ function handleTouchTap() {
     rotateShape(); // Rotate the shape
 }
 
-function generateAShape() {
-    const segment = A_SHAPE_COLOR_INDEX;
-    return [
-        [segment, segment, segment, segment, 0],
-        [0, segment, 0, 0, 0],
-        [0, , 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]
-    ];
-}
-
 function getRandomSegment() {
     return Math.floor(Math.random() * SHAPES_COLORS.length);
-}
-
-function generateSquareShape() {
-    const segment = SQUARE_COLOR_INDEX;
-    return [
-        [0, 0, segment, segment, 0],
-        [0, 0, segment, segment, 0],
-        [0, 0, segment, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]
-    ];
-}
-
-function generateBShape() {
-    const segment = B_SHAPE_COLOR_INDEX;
-    return [
-        [segment, segment, segment, 0, 0],
-        [0, 0, segment, 0, 0],
-        [0, 0, segment, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]
-    ];
-}
-
-function generateCShape() {
-    const segment = C_SHAPE_COLOR_INDEX;
-    return [
-        [0, 0, segment, segment, 0],
-        [0, 0, segment, segment, 0],
-        [0, 0, 0, segment, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]
-    ];
-}
-
-function generateDShape() {
-    const segment = D_SHAPE_COLOR_INDEX;
-    return [
-        [0, segment, 0, 0, 0],
-        [segment, segment, segment, 0, 0],
-        [0, segment, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]
-    ];
-}
-
-function generateEShape() {
-    const segment = E_SHAPE_COLOR_INDEX;
-    return [
-        [segment, 0, 0, 0, 0],
-        [segment, 0, 0, 0, 0],
-        [segment, segment, segment, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]
-    ];
-}
-
-function generateFShape() {
-    const segment = F_SHAPE_COLOR_INDEX;
-    return [
-        [0, 0, segment, 0, 0],
-        [0, 0, segment, 0, 0],
-        [segment, segment, segment, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]
-    ];
-}
-
-function generateGShape() {
-    const segment = G_SHAPE_COLOR_INDEX;
-    return [
-        [segment, segment, 0, 0, 0],
-        [segment, 0, 0, 0, 0],
-        [segment, 0, 0, 0, 0],
-        [segment, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]
-    ];
-}
-
-
-function generateTShape() {
-    const segment = T_SHAPE_COLOR_INDEX;
-    return [
-        [0, segment, 0, 0, 0],
-        [segment, segment, segment, 0, 0],
-        [segment, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]
-    ];
-}
-
-function generateZShape() {
-    const segment = Z_SHAPE_COLOR_INDEX;
-    return [
-        [segment, segment, 0, 0, 0],
-        [0, segment, 0, 0, 0],
-        [0, segment, 0, 0, 0],
-        [0, segment, 0, 0, 0],
-        [0, 0, 0, 0, 0]
-    ];
-}
-
-
-function generateSShape() {
-    const segment = S_SHAPE_COLOR_INDEX;
-    return [
-        [0, segment, segment, 0, 0],
-        [segment, segment, 0, 0, 0],
-        [0, segment, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]
-    ];
-}
-
-function generateUShape() {
-    const segment = U_SHAPE_COLOR_INDEX;
-    return [
-        [segment, 0, segment, 0, 0],
-        [segment, segment, segment, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]
-    ];
 }
 
 function generateRandomShape() {
